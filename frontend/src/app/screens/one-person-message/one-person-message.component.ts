3b41537292377a681d4ba84767b5fa1e67733d2e
Meghan Sun <meghansun322@gmail.com>
2023-04-16 16:44:16 -0400

    message screen when only one person approaches

---
+++ frontend/src/app/screens/one-person-message/one-person-message.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-one-person-message',
+  templateUrl: './one-person-message.component.html',
+  styleUrls: ['./one-person-message.component.css']
+})
+export class OnePersonMessageComponent {
+
+}
