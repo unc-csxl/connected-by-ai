8d12e4e0431cc9c4cff4467be93d81f605ea38bb
Kris Jordan <kris@cs.unc.edu>
2023-04-25 16:35:43 -0400

    Tweaks in prod

--- frontend/src/app/art-generator.service.ts
+++ frontend/src/app/art-generator.service.ts
@@ -63,7 +63,7 @@ export class ArtGeneratorService {
   constructor(private httpClient: HttpClient) {}
 
   generate(prompt: string, negative_prompt: string): void {
-    let steps = 50;
+    let steps = 48;
     this.updateState({state: 'generating', progress: 0.0, step: 0, steps: steps, image: null});
 
     let requestObservable = this.httpClient.post<Txt2ImgResult>('/sdapi/v1/txt2img', 
